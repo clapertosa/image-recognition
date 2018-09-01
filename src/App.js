550fbecd4b9b3e6ecab05a87645b78f7e1466216
clapertosa <the_wolf91@libero.it>
2018-09-01 17:47:31 +0200

    Initial Setup

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return <div>App.js</div>;
+  }
+}
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,115 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "development",
+  devtool: "cheap-module-eval-source-map",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    chunkFilename: "[id].js",
+    publicPath: ""
+  },
+  module: {
+    rules: [
+      // JS and JSX
+      {
+        test: /\.(js|jsx)$/,
+        loader: "babel-loader",
+        exclude: /node_modules/
+      },
+      // CSS
+      {
+        oneOf: [
+          {
+            resourceQuery: /^\?raw$/,
+            use: [
+              MiniCssExtractPlugin.loader,
+              {
+                loader: "css-loader",
+                options: { localIdentName: "external-[name]" }
+              }
+            ]
+          }
+        ]
+      },
+      {
+        test: /\.(css|sass|scss)$/,
+        exclude: /node_modules/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+              modules: true,
+              localIdentName: "[path][name]__[local]--[hash:base64:5]"
+            }
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins: [
+                require("autoprefixer")({
+                  browsers: ["> 1%", "last 2 versions"]
+                })
+              ]
+            }
+          },
+          {
+            loader: "sass-loader"
+          }
+        ]
+      },
+      // FONTS
+      {
+        test: /\.(woff|woff2|eot|ttf)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "fonts/[name].[ext]"
+            }
+          }
+        ]
+      },
+      // IMAGES
+      {
+        test: /\.(jpg|jpeg|png|gif|svg)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "images/[name].[ext]"
+            }
+          }
+        ]
+      }
+    ]
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        styles: {
+          name: "styles",
+          test: /\.css$/,
+          chunks: "all",
+          enforce: true
+        }
+      }
+    }
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    }),
+    new HtmlWebpackPlugin({
+      template: __dirname + "/src/index.html",
+      filename: "index.html",
+      inject: "body"
+    })
+  ]
+};
