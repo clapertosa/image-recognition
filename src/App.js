c558780f421b17666f4b9a442d0b664d009a9471
clapertosa <the_wolf91@libero.it>
2018-09-05 21:32:42 +0200

    Redux implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
+import { Switch, Route, withRouter } from "react-router-dom";
+import { Home, Login, Signup } from "./components/pages/";
 import Layout from "./hoc/Layout/Layout";
 
 class App extends Component {
   render() {
-    return <Layout />;
+    let routes = (
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/" exact component={Home} />
+        <Route component={Home} />
+      </Switch>
+    );
+
+    return <Layout>{routes}</Layout>;
   }
 }
-export default App;
+export default withRouter(App);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import signupReducer from "./store/reducers/signup";
+import { BrowserRouter } from "react-router-dom";
 import "./index.scss";
 import App from "./App";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+const composeEnhancers =
+  process.env.NODE_ENV === "development"
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+    : null || compose;
+
+const rootReducer = combineReducers({
+  signup: signupReducer
+});
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.querySelector("#root")
+);
