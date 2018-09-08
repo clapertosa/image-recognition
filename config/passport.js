a3dcb3845e585d4aac24be0852fc3b73df8c1380
clapertosa <the_wolf91@libero.it>
2018-09-08 20:33:51 +0200

    Initialize JWT Authentication with Passport

---
+++ config/passport.js
@@ -0,0 +1,25 @@
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const knex = require("../db/knex");
+const keys = require("./keys");
+
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = keys.secret;
+
+module.exports = passport => {
+  passport.use(
+    new JwtStrategy(opts, (jwt_payload, done) => {
+      knex("users")
+        .select()
+        .where("id", jwt_payload.id)
+        .then(user => {
+          if (user[0]) {
+            return done(null, user[0]);
+          }
+          return done(null, false);
+        })
+        .catch(error => res.status(400).json(error));
+    })
+  );
+};
--- server.js
+++ server.js
@@ -4,9 +4,12 @@ const morgan = require("morgan");
 const compression = require("compression");
 const path = require("path");
 const bodyParser = require("body-parser");
+const passport = require("passport");
 
 //API ROUTES
 const signup = require("./routes/api/signup");
+const login = require("./routes/api/login");
+const user = require("./routes/api/user");
 
 const PORT = process.env.PORT || 3000;
 
@@ -23,6 +26,14 @@ app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
+
 app.use("/api/signup", signup);
+app.use("/api/login", login);
+app.use("/api/user", user);
 
 app.listen(PORT, () => console.log("Server started"));
