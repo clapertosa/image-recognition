7ff06fbeccd10dfdc1da9337e382d50928f4b046
clapertosa <the_wolf91@libero.it>
2018-09-08 20:55:27 +0200

    Automatically set the current user or logout if there is no token in localStorage

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import jwtDecode from "jwt-decode";
 import { createStore, applyMiddleware, combineReducers } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import signupReducer from "./store/reducers/signup";
+import loginReducer from "./store/reducers/login";
+import authReducer from "./store/reducers/auth";
+import { setCurrentUser, logout } from "./store/actions/auth";
 import { BrowserRouter } from "react-router-dom";
 import "./index.scss";
 import App from "./App";
@@ -14,13 +18,25 @@ const composeEnhancers =
     : null || compose;
 
 const rootReducer = combineReducers({
-  signup: signupReducer
+  signup: signupReducer,
+  login: loginReducer,
+  auth: authReducer
 });
 
 const store = createStore(
   rootReducer,
   composeEnhancers(applyMiddleware(thunk))
 );
+
+if (localStorage.token) {
+  const user = jwtDecode(localStorage.token);
+  const currentTime = Date.now() / 1000;
+  if (user.exp < currentTime) {
+    store.dispatch(logout());
+  } else {
+    store.dispatch(setCurrentUser(user));
+  }
+}
 
 ReactDOM.render(
   <Provider store={store}>
