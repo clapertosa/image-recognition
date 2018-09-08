ece87d4cc1eae28b47cda0b01ef5860744bf69bb
clapertosa <the_wolf91@libero.it>
2018-09-08 20:41:38 +0200

    LoginForm and page implemented

--- src/components/Form/LoginForm/LoginForm.js
+++ src/components/Form/LoginForm/LoginForm.js
@@ -1,9 +1,130 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as actions from "../../../store/actions/login";
+import { Typography, TextField, Button } from "@material-ui/core";
+
+import styles from "./LoginForm.scss";
 
 class LoginForm extends Component {
+  state = {
+    user: {
+      email: "",
+      password: ""
+    },
+    emailError: "",
+    passwordError: ""
+  };
+
+  onChangeHandler(e) {
+    const userState = { ...this.state.user };
+    userState[e.target.name] = e.target.value;
+    this.setState({ user: userState });
+  }
+
+  onSubmitHandler = e => {
+    e.preventDefault();
+    if (!this.validate()) {
+      this.props.login(this.state.user);
+    }
+  };
+
+  validate = () => {
+    let error = false;
+    let errors = {
+      emailError: ""
+    };
+
+    if (this.state.user.email.length <= 0) {
+      error = true;
+      errors.emailError = "Email is required";
+    }
+
+    if (
+      this.state.user.email.length > 0 &&
+      !this.state.user.email.match(
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      )
+    ) {
+      error = true;
+      errors.emailError = "Email is not valid";
+    }
+
+    if (this.state.user.password.length <= 0) {
+      error = true;
+      errors.passwordError = "Password is required";
+    }
+
+    this.setState({ ...errors });
+
+    return error;
+  };
+
   render() {
-    return <div>LoginForm</div>;
+    return (
+      <div className={styles["form-container"]}>
+        <form onSubmit={e => this.onSubmitHandler(e)}>
+          <Typography color="textPrimary" variant="display1">
+            Login
+          </Typography>
+          <TextField
+            fullWidth
+            margin="normal"
+            onChange={e => this.onChangeHandler(e)}
+            name="email"
+            label="Email"
+            type="text"
+            helperText={this.state.emailError}
+            error={this.state.emailError ? true : false}
+            value={this.state.user.email}
+          />
+
+          <TextField
+            fullWidth
+            margin="normal"
+            onChange={e => this.onChangeHandler(e)}
+            name="password"
+            label="Password"
+            type="password"
+            helperText={this.state.passwordError}
+            error={this.state.passwordError ? true : false}
+            value={this.state.user.password}
+          />
+
+          {this.props.loginError ? (
+            <span className={styles["error-message"]}>
+              {this.props.loginError}
+            </span>
+          ) : null}
+
+          <Button
+            className={styles.button}
+            size="medium"
+            variant="contained"
+            color="primary"
+            type="submit"
+          >
+            Submit
+          </Button>
+        </form>
+      </div>
+    );
   }
 }
 
-export default LoginForm;
+const mapStateToProps = state => {
+  return {
+    loginSuccess: state.login.success,
+    loginError: state.login.error
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    login: user => dispatch(actions.login(user))
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(LoginForm);
--- src/components/pages/Login/Login.js
+++ src/components/pages/Login/Login.js
@@ -1,6 +1,7 @@
 import React from "react";
+import { LoginForm } from "../../../components";
 
 const Login = () => {
-  return <div>Login</div>;
+  return <LoginForm />;
 };
 export default Login;
