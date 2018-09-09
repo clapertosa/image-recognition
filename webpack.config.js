9dcb9b1f58284e9c500c925f932ec58ff2d76f6e
clapertosa <the_wolf91@libero.it>
2018-09-09 18:36:10 +0200

    Set publicPath to absolute path

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
     chunkFilename: "[id].js",
-    publicPath: ""
+    publicPath: "/"
   },
   module: {
     rules: [
