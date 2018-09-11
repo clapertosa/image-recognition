a74e118e39ced3c8a3eaa1fc466a983e43593878
clapertosa <the_wolf91@libero.it>
2018-09-11 16:14:14 +0200

    Add cloudmersive api key

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,5 @@
 module.exports = {
   secret: process.env.SECRET_OR_KEY,
-  sendgridApiKey: process.env.SENDGRID_API_KEY
+  sendgridApiKey: process.env.SENDGRID_API_KEY,
+  cloudmersiveApiKey: process.env.CLOUDMERSIVE_API_KEY
 };
