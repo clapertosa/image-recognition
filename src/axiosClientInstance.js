48f87e7a65d2aec3ea2389d5b9b51ccf007f4667
clapertosa <the_wolf91@libero.it>
2018-09-16 19:20:01 +0200

    Rename axios instance for client

--- src/axiosClientInstance.js
+++ src/axiosClientInstance.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-const token = localStorage.token;
 const instance = axios.create({
   baseURL: `http${process.env.NODE_ENV === "production" ? "s" : ""}://${
     window.location.host
