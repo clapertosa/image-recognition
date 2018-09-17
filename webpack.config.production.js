0212fc730c9e5b027b048a6a5a2ca6b2aa24a4f6
clapertosa <the_wolf91@libero.it>
2018-09-17 17:44:15 +0200

    Enable sideEffects in package.json and .babelrc to improve bundle size

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -1,12 +1,26 @@
 const merge = require("webpack-merge");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const commonConfig = require("./webpack.config");
 
 module.exports = merge.smart(commonConfig, {
   mode: "production",
   optimization: {
-    minimizer: [new UglifyJsPlugin({}), new OptimizeCSSAssetsPlugin({})]
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          mangle: true,
+          compress: {
+            drop_console: true,
+            drop_debugger: true
+          }
+        }
+      }),
+      new OptimizeCSSAssetsPlugin({
+        cssProcessorPluginOptions: {
+          preset: ["default", { discardComments: { removeAll: true } }]
+        }
+      })
+    ]
   }
 });
