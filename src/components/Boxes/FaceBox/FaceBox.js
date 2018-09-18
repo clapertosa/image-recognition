33815da1e739eda83039f67233f05ae373689028
clapertosa <the_wolf91@libero.it>
2018-09-18 19:13:00 +0200

    Create a bounding-box container to set them to absolute

--- src/components/Boxes/FaceBox/FaceBox.js
+++ src/components/Boxes/FaceBox/FaceBox.js
@@ -5,16 +5,18 @@ const FaceBox = props => {
   return props.data.map((faceCoords, index) => {
     let coords = calculateBoxPosition(faceCoords);
     return (
-      <div
-        key={index}
-        className={styles.box}
-        style={{
-          top: coords.top + "%",
-          right: coords.right + "%",
-          bottom: coords.bottom + "%",
-          left: coords.left + "%"
-        }}
-      />
+      <div className={styles["box-container"]}>
+        <div
+          key={index}
+          className={styles.box}
+          style={{
+            top: coords.top + "%",
+            right: coords.right + "%",
+            bottom: coords.bottom + "%",
+            left: coords.left + "%"
+          }}
+        />
+      </div>
     );
   });
 };
@@ -26,9 +28,9 @@ const calculateBoxPosition = faceCoords => {
 
   return {
     left: (faceCoords.LeftX / width) * 100,
-    top: (faceCoords.TopY / height) * 100,
+    top: (faceCoords.TopY / height) * 100 - 1,
     right: ((width - faceCoords.RightX) / width) * 100,
-    bottom: ((height - faceCoords.BottomY) / height) * 100
+    bottom: ((height - faceCoords.BottomY) / height) * 100 + 1
   };
 };
 
