ee364177257c5722a9b0fff67fd790d611315235
clapertosa <the_wolf91@libero.it>
2018-09-24 13:01:45 +0200

    Add an animation to Caption component

--- src/components/Caption/Caption.js
+++ src/components/Caption/Caption.js
@@ -1,10 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import styles from "./Caption.scss";
 
 const Caption = props => {
   return (
-    <div className={styles.caption}>
+    <div className={[styles.caption, styles["enter-from-left"]].join(" ")}>
       {getCaptionType(props.type, props.data)}
     </div>
   );
